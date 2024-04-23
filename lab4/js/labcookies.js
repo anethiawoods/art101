b2a2489d6fe0c8d31df30d74d8080d56dd2499c6
L <ltachiba@ucsc.edu>
2024-04-23 15:21:15 -0700

    lab 4 stuff

---
+++ lab4/js/labcookies.js
@@ -0,0 +1,58 @@
+// Baking_cookies
+// Instructions on baking cookies
+// Author: L Tachibana
+// Date: 23 April 2024
+
+/* 
+Google ‘recipe for cookies’
+Go to the pantry
+    Check pantry for all necessary ingredients & tools 
+        Necessary ingredients: flour, sugar, unsalted butter, baking soda, chocolate chips, eggs, vanilla extract, salt
+        Necessary tools:2 large bowls, whisk, baking pan, parchment paper, measuring cups
+    If missing ingredient or tool, go to the store
+        Locate needed ingredients or tools in store
+        Place ingredients or tools in basket
+        Go to check-out counter
+        Pay for items
+        Go home with ingredients or tools
+    If all ingredients and tools are obtained, place them on the kitchen counter
+Measure ingredients according to recipe, match the required amounts of each ingredient by name and amount 
+    Example: If recipe states ¾ cup of flour then measure ¾ cup of flour
+Categorize and separate wet and dry ingredients
+    Wet ingredients: sugar, unsalted butter, eggs, vanilla extract
+    Dry ingredients: flour, salt, baking soda, and chocolate chips 
+According to the measured amounts, place the dry ingredients into the large bowl
+According to the measured amounts, place the wet ingredients into a separate bowl 
+    Use whisk to beat the mixture until doubled the size and evenly incorporated
+    Ensure the eggs are cracked with the yolk and white added to the wet mixture and the shells discarded
+Add dry ingredient mixture into the bowl containing the wet mixture. 
+Mix both the wet and dry ingredients with the whisk until the mixture reaches an even consistency
+    Even consistency is indicated by all ingredients except for the chocolate chips to not have observable streaks or other distinct patches from one another in the mixture. 
+    Chocolate chips should be distributed evenly throughout the overall mixture
+Remove whisk from the overall mixture
+Place the mixture into the fridge
+Preheat the oven to 350F
+Put away the excess ingredients into the pantry
+When the oven reaches 350F take the cookie mix out of the fridge
+Line the baking pan with parchment paper
+Wash hands and then dry hands
+Scoop out handful-sized dollops of cookie mix onto the parchment paper in equal portions until the mixture runs out 
+    Make sure the dollops are distributed evenly throughout the pan 
+    From the pan’s borders, ensure there is a ½ inch margin between the dollops of cookie mix and the sides of the pan
+    Limit the amount of dollops on the pan to be 8 total. If cookie mix remains, run the dollops over to a new pan prepared the same way as the previous 
+Place the baking pan with unbaked cookies into the heated oven
+    If more than one pan is created, place in the oven as well within the oven rack’s area while not overlapping the pans over another 
+Wait for 20 minutes for the cookie mixture to transform into cookies
+Clean up while waiting
+    Wipe the kitchen counter table with a rag and soap
+    Take the used tools to the sink
+    Wash tools with water and soap until no longer greasy
+    Place washed tools on drying rack
+Take the pan out of the oven
+    pan is created, take the excess pans out of the oven as well 
+Turn off the oven 
+Let cookies cool for 20 minutes. Afterwards, remove the cookies from the pan 
+Eat the cookies until full
+Place leftover cookies into a box
+Give box to a deserving friend 
+*/
