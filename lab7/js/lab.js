2db3eabc1235e6d4c7e84073da2ffb3025001b9f
L <ltachiba@ucsc.edu>
2024-05-01 01:10:25 -0700

    lab7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,21 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* Lab 7 - Functions
+Using functions to retrieve and sort a user's name
+Author: L Tachibana
+Date: 1 May 2024 */
 
-// Constants
+//Creating a function to retrieve user's name & sort the letters
+function sortName (){
+  //retrieve user's name
+  let userName = window.prompt("What's your name?");
+  console.log(userName);
+  //sort and return user's name
+  let sortedName = userName.split('').sort().join('');
+  console.log(sortedName);
+  //return sorted name
+  return sortedName;
+};
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Actually your name is ",sortName());
