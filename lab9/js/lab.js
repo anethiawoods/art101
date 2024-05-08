9340eb522cfddaa363728f66d3dc88eb12583467
L <ltachiba@ucsc.edu>
2024-05-08 15:25:37 -0700

    lab9 css

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,11 +3,15 @@
 Author: L Tachibana
 Date: ---- 2024 */
 
-//figure out what theyll do later... maybe... lets see !
+//figure out what theyll do later... maybe... lets see ! maybe here can toggle on and off challenge section
 $("#button-challenge")
 
-//
+//maybe change da colour (cycle through colours?)
+    //ok apparently this requires conditionals. LOL. maybe. do later....
 $("#button-problem")
 
-//
+//ooh maybe add a picture ?
+//need 2 figure out how to make not infinitely generating pics
 $("#button-results")
+
+//also do a thing when hover over text changes colour/text!!!
