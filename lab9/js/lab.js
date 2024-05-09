b9fe24dddff3bbae0b953e9c533abb7d3588c65f
L <ltachiba@ucsc.edu>
2024-05-08 20:22:27 -0700

    lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,17 +1,42 @@
 /* Lab 9 - Libraries & jQuery
-----
+Using jQuery to make buttons change elements of the site when clicked
 Author: L Tachibana
-Date: ---- 2024 */
+Date: 8 May 2024 */
 
-//figure out what theyll do later... maybe... lets see ! maybe here can toggle on and off challenge section
-$("#button-challenge")
+//Add buttons to each section
+    $("#challenges").append("<button id='challenges-button'>Eh? What's here...</button>");
+    $("#problems").append("<button id='problems-button'>Make me look real weird</button>");
+    $("#results").append("<button id='results-button'>Say hi sol :)</button><button id='unresults-button'>Take him away!</button>");
 
-//maybe change da colour (cycle through colours?)
-    //ok apparently this requires conditionals. LOL. maybe. do later....
-$("#button-problem")
+//Button for challenge section - Make it toggle on/off the p text
+    // function to change challenges section class
+    function toggleText () {
+        $("#challenges p").toggleClass("challenge-special");
+    };
 
-//ooh maybe add a picture ?
-//need 2 figure out how to make not infinitely generating pics
-$("#button-results")
+    //when button is clicked, it will toggle the challenges section to be visible
+    $("#challenges-button").click(toggleText);
 
-//also do a thing when hover over text changes colour/text!!!
+//Button for problems section - when button is clicked, it will change the  css
+    $("#problems-button").click(function(){
+        $("#problems p").toggleClass("problems-special");
+    }
+    );
+
+//Button for results section - Add picture
+     //when button is clicked, it will add pic
+    $("#results-button").click(function(){
+        $("#img-line").append("<img class='solling' src='./img/solling-round.png'>");
+    });
+    
+//Button for results section - Remove picture
+     //when button is clicked, it will removE pic
+    $("#unresults-button").click(function(){
+        $(".solling").remove();
+    });
+    
+//Hover over headers to change colour
+  //when button is clicked, turns pink
+    $("h1").hover(function(){
+        $("h1").toggleClass("h1-special");
+    });
