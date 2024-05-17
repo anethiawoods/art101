39d471012c1e88d72b9d5504ef974233fb911dd6
L <ltachiba@ucsc.edu>
2024-05-17 15:55:25 -0700

    lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,4 +1,21 @@
-/* Lab 10 - JavaScript for the Web
-
+/* Lab 11 - JavaScript Events and Forms
+Taking a value from and input field, sorting it, and then putting it on the page
 Author: L Tachibana
-Date: 9 May 2024 */
+Date: 17 May 2024 */
+
+
+
+
+
+//Defining a function that retrieve user's name & sort the letters
+function sortName (){
+    //retrieve user's name
+    let userName = $("#input-here").val()
+    //sort and return user's name
+    let sortedName = userName.split('').sort().join('');
+    //return sorted name
+    $("#output-name").html("Your new name is: "+ sortedName+"!");
+  };
+  
+  //when button is clicked, it will run function & output sorted name
+  $("#my-button").click(sortName);
