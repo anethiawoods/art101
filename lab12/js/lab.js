4d7acc360639f90172716a7391103c8c539946df
L <ltachiba@ucsc.edu>
2024-06-02 03:16:32 -0700

    idk

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,4 +1,40 @@
-/* Lab 10 - JavaScript for the Web
-
+/* Lab 12 - Conditionals
+Working with conditionals to sort people into categories based on input
 Author: L Tachibana
-Date: 9 May 2024 */
+Date: 30 May 2024 */
+
+// //Creating a function to retrieve user's name
+// function sorting (){
+//     // let str = $("#input").val();
+    // len = str.length;
+    // mod = len%6;
+    // if (mod==0){
+    //     console.log("Ranger");
+    //     return "Ranger";
+    // }
+    // else if (mod==1){
+    //     console.log("Ranger");
+    //     return "Barbarian"
+    // }
+    // else if (mod==2){
+    //     console.log("Ranger");
+    //     return "Magician"
+    // }
+    // else if (mod==3){
+    //     console.log("Ranger");
+    //     return "Thief"
+    // }
+    // else if (mod==4){
+    //     console.log("Ranger");
+    //     return "Cavalier"
+    // }
+    // else if (mod==5){
+//     //     console.log("Ranger");
+//     //     return "Acrobat"
+//     // }
+// }
+
+//put it up on the site when button clickes
+$("#button").click(function(){
+    console.log("hi");
+});
